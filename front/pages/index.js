968ec44f948669633891fd7b7be2eac52b641bc9
Taewoong Moon <moondingie5@gmail.com>
2019-09-24 02:11:35 +0900

    antd css 적용 안됨. 해결요망

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,8 +1,20 @@
+import AppLayout from '../components/AppLayout';
+import Link from 'next/link';
+import Head from 'next/head';
+
 const Home = () => {
   return (
-    <div>
-      HEllo, NExt
-    </div>
+    <>
+      <Head>
+        <title>NodeBird</title>
+        {/* <link rel="https://cdnjs.cloudflare.com/ajax/libs/antd/3.23.4/antd.css" /> */}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/antd/3.23.4/antd.css"></script>
+      </Head>
+      <AppLayout>
+        <Link href="/about"><a>About</a></Link>
+        <div>Hello, Next!</div>
+      </AppLayout>
+    </>
   );
 };
 
