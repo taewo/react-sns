ad302c33602add76c4916fe29c0c8baa4b93380d
Taewoong Moon <moondingie5@gmail.com>
2019-10-10 15:34:36 +0900

    #4-2 사가 미들웨어 리덕스에 연결하기

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -6,6 +6,8 @@ import rootReducer from '../reducers'
 import { Provider } from 'react-redux'
 import { createStore, compose, applyMiddleware } from 'redux'
 import withRedux from 'next-redux-wrapper'
+import createSagaMiddleware from 'redux-saga'
+import rootSaga from '../sagas'
 
 const NodeBird = ({ Component, store }) => {
 	return (
@@ -26,11 +28,15 @@ NodeBird.propTypes = {
 }
 
 export default withRedux((initialState, options) => {
-	const middlewares = []
-	const enhancer = compose(
+	const sagaMiddleware = createSagaMiddleware()
+	const middlewares = [sagaMiddleware]
+	const enhancer = process.env.NODE_ENV === 'production'
+	? compose(applyMiddleware(...middlewares))
+	:	compose(
 		applyMiddleware(...middlewares),
 		!options.isServer && window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
 	)
 	const store = createStore(rootReducer, initialState, enhancer)
+	sagaMiddleware.run(rootSaga)
 	return store
 })(NodeBird)
